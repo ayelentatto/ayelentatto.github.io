7292fa28dc74e3e9a5637949fb3a725060f75f45
ayelentatto <110647975+ayelentatto@users.noreply.github.com>
2023-02-06 16:57:34 -0300

    version 1
    finish a first versión

---
+++ aboutme.js
@@ -0,0 +1,9 @@
+const btnJap = document.getElementById("JaP1");
+
+verRubrica()
+
+function verRubrica() {
+  btnJap.addEventListener('click', () => {
+    abrirNuevoTab("https://drive.google.com/file/d/1-raFaU9XfRYG9GMOUfHvVV7GTIAkH8lw/view?usp=sharing")
+  })
+}
